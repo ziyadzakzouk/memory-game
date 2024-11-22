fd86b119831a7400e04de1c0c9fe072c6cb90acb
zeyad zakzouk <ziyad.zakzouk@outlook.com>
2024-11-22 17:00:00 +0200

    make the cards shuffle between numbers from 1 to 6

--- game/Main.cpp
+++ game/Main.cpp
@@ -146,7 +146,7 @@ delete[] cards;
 void Deck::shuffle(){
 
     Card cardPairs[16];
-    for (int i = 0; i < 8; ++i) {
+    for (int i = 0; i < 5; ++i) {
         Card card;
         card.setNumber(i);
         cardPairs[2 * i] = card;
